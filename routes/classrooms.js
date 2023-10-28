69e1edc24dd28a0d4b5e0da8938391bbcfbd9354
Suyash Saxena <suyashsincever13@gmail.com>
2023-10-28 18:59:40 +0530

    Added the api for adding student to the classroom

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -5,6 +5,17 @@ const classRoomController = require('../controllers/classRoomController');
 router.post('/create',checkAuth,(req,res)=>{
     classRoomController.createClass(req,res);
 });
+router.put('/edit',checkAuth,(req,res)=>{
+    classRoomController.editClass(req,res);
+});
+
+router.delete('/delete',checkAuth,(req,res)=>{
+    classRoomController.deleteClass(req,res);
+});
+
+router.post('/add/student',checkAuth,(req,res)=>{
+    classRoomController.addStudent(req,res);
+})
 
 
 module.exports = router;
