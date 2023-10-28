bdd9a26f6f11505e1e60db332d37b1b6bbcddea2
Suyash Saxena <suyashsincever13@gmail.com>
2023-10-29 04:38:05 +0530

    Added api for getting the files

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -2,7 +2,7 @@ const logger = require('../logger');
 const Users = require('../models/users');
 const classRoom = require('../models/classrooms');
 const Files = require('../models/files');
-
+const fs=require('fs');
 class filesController{
 
     async uploadFile(req,res){
@@ -10,24 +10,50 @@ class filesController{
             const {file_name,classroom_id,file_type,description,file}=req.body;
             const user_id=req.userData.user_id;
             const user_name = req.userData.user_name;
+            const uploadedFile = req.file;
             if(!file_name){
+                if(uploadedFile){
+                    console.log(uploadedFile.path);
+                    fs.unlinkSync(uploadedFile.path);
+                }
                 return res.status(400).json({message:"File Name Is Required",data:[]});
             }
             if(!req.file && !file){
                 return res.status(400).json({message:"File Is Required",data:[]});
             }
             if(!file_type){
+                if(uploadedFile){
+                    console.log(uploadedFile.path);
+                    fs.unlinkSync(uploadedFile.path);
+                }
                 return res.status(400).json({message:"File Type Is Required",data:[]});
             }
             if(!classroom_id){
+                if(uploadedFile){
+                    console.log(uploadedFile.path);
+                    fs.unlinkSync(uploadedFile.path);
+                }
                 return res.status(400).json({message:"ClassRoom Id is Requried",data:[]});
             }
             const supportedFileTypes =['AUDIO','VIDEO','IMAGE','URL'];
             if(!supportedFileTypes.includes(file_type.toUpperCase())){
+                if(uploadedFile){
+                    console.log(uploadedFile.path);
+                    fs.unlinkSync(uploadedFile.path);
+                }
                 return res.status(400).json({message:`FILE TYPE NOT SUPPORTED`,data:`SUPPORTED FILE TYPES ARE ${supportedFileTypes}` });
             }
             const file_path = req.uploadedFilePath||file;
             console.log(file_path);
+            const validTutor = await classRoom.findOne({_id:classroom_id,"created_by.user_id":user_id});
+            console.log(validTutor);
+            if(!validTutor){
+                if(uploadedFile){
+                    console.log(uploadedFile.path);
+                    fs.unlinkSync(uploadedFile.path);
+                }
+                return res.status(400).json({message:"You can upload file to your own class",data:[]});
+            }
             const newFile = new Files({
                 file_name,
                 classroom_id,
@@ -51,6 +77,7 @@ class filesController{
         try{
             const {description,file_name,file_type,file_id}=req.body;
             const user_id=req.userData.user_id;
+            const uploadedFile =req.file;
             if(!file_id){
                 return res.status(400).json({message:"File ID Is Requried",data:[]});
             }
@@ -96,6 +123,53 @@ class filesController{
             logger.error(error);
             return res.status(500).json({message:"Something Went Wrong",data:[]});
         }
+    }
+
+    async getFiles(req,res){
+        try{
+            const user_id= req.userData.user_id;
+            const role_id = req.userData.role_id;
+            if(role_id==1){
+                console.log('Getting the files for the tutor');
+                var files = await this.getFilesForTutor(user_id);
+            }else{
+                console.log("Getting files for the student");
+                var files = await this.getFilesForStudent(user_id);
+            }
+            
+            return res.status(200).json({message:"Success",data:files});
+
+        }catch(error){
+            logger.error(error);
+            return res.status(500).json({message:"Something Went Wrong",data:[]});
+        }
+    }
+
+
+    async getFilesForTutor(user_id){
+        try{
+            const files = await Files.find({"uploaded_by.user_id":user_id});
+            return files;
+        }catch(error){
+            logger.error(error);
+            throw {status:500,message:error.message||"Something Went Wrong"};
+        }
+    }
+
+    async getFilesForStudent(user_id){
+        try{
+            const classrooms= await classRoom.find({"students.student_user_id":user_id});
+            console.log(classrooms);
+            const classroom_ids = classrooms.map(classroom => classroom._id.toString());
+            console.log(classroom_ids);
+            const files = await Files.find({classroom_id:{$in:classroom_ids}});
+            console.log(files);
+            return files;
+        }catch(error){
+            logger.error(error);
+            throw {status:500,message:error.message||"Something Went Wrong"};
+        }
+
     }
 
 }
